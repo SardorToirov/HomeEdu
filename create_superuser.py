0058fd91729832fa9e5ae8412634f3e85e3ad94e
SardorToirov <toirovsardor62@gmail.com>
2026-03-14 20:58:25 +0500

    fix whitenoise static

--- create_superuser.py
+++ create_superuser.py
@@ -1,16 +1,21 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
+if not User.objects.filter(email="admin@gmail.com").exists():
     User.objects.create_superuser(
-        username="admin",
         email="admin@gmail.com",
-        password="admin123"
+        password="admin123",
+        first_name="Admin",
+        last_name="User"
     )
+
+    print("Superuser created")
+else:
+    print("Superuser already exists")
